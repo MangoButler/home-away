7d365b44dfa4fbea189f3a5a80ef036bd6be9e0a
Christian Office <christian.hg.hegi@gmail.com>
2024-09-03 08:50:31 +0900

    with update profile action and profile page

--- utils/actions.ts
+++ utils/actions.ts
@@ -6,6 +6,22 @@ import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("You must be logged in to access this content.");
+  }
+  if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+  return user;
+};
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : "An error occured!",
+  };
+};
+
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
@@ -29,9 +45,7 @@ export const createProfileAction = async (
       },
     });
   } catch (error) {
-    return {
-      message: error instanceof Error ? error.message : "There was an error",
-    };
+    return renderError(error);
   }
   redirect("/");
 };
@@ -49,4 +63,43 @@ export const fetchProfileImage = async () => {
   });
 
   return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+  if (!profile) redirect("/profile/create");
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = profileSchema.safeParse(rawData);
+    console.log(validatedFields);
+
+    if (!validatedFields.success) {
+      const errors = validatedFields.error.errors.map((error) => error.message);
+      throw new Error(errors.join(", "));
+    }
+
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: validatedFields.data,
+    });
+    revalidatePath("/profile");
+    return { message: "Profile updated successfully!" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
--- utils/schemas.ts
+++ utils/schemas.ts
@@ -4,14 +4,14 @@ import { ZodSchema } from "zod";
 export const profileSchema = z.object({
   firstName: z
     .string()
-    .max(15, { message: "Maximum of 15 characters allowed" })
-    .min(2, { message: "Needs to be at least two characters long" }),
+    .max(15, { message: "Maximum of 15 characters allowed for first name" })
+    .min(2, { message: "First name needs to be at least two characters long" }),
   lastName: z
     .string()
-    .max(15, { message: "Maximum of 15 characters allowed" })
-    .min(2, { message: "Needs to be at least two characters long" }),
+    .max(15, { message: "Maximum of 15 characters allowed for last name" })
+    .min(2, { message: "Last name needs to be at least two characters long" }),
   username: z
     .string()
-    .max(15, { message: "Maximum of 15 characters allowed" })
-    .min(2, { message: "Needs to be at least two characters long" }),
+    .max(15, { message: "Maximum of 15 characters allowed for username" })
+    .min(2, { message: "Username needs to be at least two characters long" }),
 });
