c414641c161bf38e7ad79feb0357b2310214b41d
Chris <christian.hegi99@gmail.com>
2024-09-09 08:51:45 +0900

    with property input

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -3,6 +3,10 @@ import FormContainer from "@/components/form/FormContainer";
 import { createPropertyAction } from "@/utils/actions";
 import { SubmitButton } from "@/components/form/Buttons";
 import PriceInput from "@/components/form/PriceInput";
+import CategoriesInput from "@/components/form/CategoriesInput";
+import TextAreaInput from "@/components/form/TextAreaInput";
+import CountriesInput from "@/components/form/CountriesInput";
+import ImageInput from "@/components/form/ImageInput";
 
 function CreatePropertyPage() {
   return (
@@ -27,9 +31,16 @@ function CreatePropertyPage() {
               defaultValue="Dream Getaway awaits you here!"
             />
             <PriceInput />
-            {/* category */}
+            <CategoriesInput />
           </div>
-          {/* textarea / description Input*/}
+          <TextAreaInput
+            name="description"
+            labelText="Description (10 - 1000 Words)"
+          />
+          <div className="grid sm:grid-cols-2 gap-8 mt-3">
+            <CountriesInput />
+            <ImageInput />
+          </div>
           <SubmitButton text="create rental" className="mt-12" />
         </FormContainer>
       </div>
