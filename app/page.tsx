c8f6fc571c3e4845f52b655a69e5f751391c102f
Christian Office <christian.hg.hegi@gmail.com>
2024-09-17 08:47:22 +0900

    with search input functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,29 @@
+import LoadingCards from "@/components/card/LoadingCards";
 import CategoriesList from "@/components/home/CategoriesList";
 import PropertiesContainer from "@/components/home/PropertiesContainer";
+import { Suspense } from "react";
 
-
-function Homepage({searchParams}:{searchParams:{category?:string, search?:string}}) {
-  
+function Homepage({
+  searchParams,
+}: {
+  searchParams: { category?: string; search?: string };
+}) {
   return (
     <section>
-      <CategoriesList 
-      category={searchParams.category}
-      search={searchParams.search}
+      <CategoriesList
+        category={searchParams.category}
+        search={searchParams.search}
       />
-      <PropertiesContainer
-      category={searchParams.category}
-      search={searchParams.search}
-      />
+      <Suspense fallback={<LoadingCards />}>
+        <PropertiesContainer
+          category={searchParams.category}
+          search={searchParams.search}
+        />
+      </Suspense>
     </section>
   );
 }
 
 export default Homepage;
+
+//wrapping the Properties container in the suspense gives the ability to only display part of the loading state in the component that is actually loading, for this to work it is important to fetch within the Properties container and not in the Homepage
