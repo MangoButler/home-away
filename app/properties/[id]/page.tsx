32fb1cd8b4192dd1d2dcd10bf4b738ca33412b27
Chris <christian.hegi99@gmail.com>
2024-09-30 07:50:32 +0900

    this is the done

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,9 +1,15 @@
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import PropertyRating from "@/components/card/PropertyRating";
+import BookingCalendar from "@/components/properties/BookingCalendar";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
+import Description from "@/components/properties/Description";
 import ImageContainer from "@/components/properties/ImageContainer";
+import PropertyDetails from "@/components/properties/PropertyDetails";
 import ShareButton from "@/components/properties/ShareButton";
+import UserInfo from "@/components/properties/UserInfo";
+import { Separator } from "@/components/ui/separator";
 import { fetchPropertyDetails } from "@/utils/actions";
+
 import { redirect } from "next/navigation";
 
 async function PropertiesDetailsPage({ params }: { params: { id: string } }) {
@@ -11,7 +17,8 @@ async function PropertiesDetailsPage({ params }: { params: { id: string } }) {
   if (!property) redirect("/");
   const { baths, bedrooms, beds, guests } = property;
   const details = { baths, bedrooms, beds, guests };
-  
+  const firstName = property.profile.firstName;
+  const profileImage = property.profile.profileImage;
   return (
     <section>
       <BreadCrumbs name={property.name} />
@@ -23,14 +30,20 @@ async function PropertiesDetailsPage({ params }: { params: { id: string } }) {
         </div>
       </header>
       <ImageContainer name={property.name} mainImage={property.image} />
-      <section className="lg:grid lg:grid-cols-12 gap-x-12 mt-12">
-        <div className="lg:col-span-8">
+      <section className="lg:grid lg:grid-cols-12 md:grid md:grid-cols-6 gap-x-12 mt-12">
+        <div className="lg:col-span-8 md:col-span-4">
           <div className="flex gap-4 items-center">
             <h1 className="text-lg font-bold">{property.name}</h1>
             <PropertyRating inPage propertyId={property.id} />
           </div>
+          <PropertyDetails details={details} />
+          <UserInfo profile={{ firstName, profileImage }} />
+          <Separator className="mt-4" />
+          <Description description={property.description} />
         </div>
-        <div className="lg:col-span-4 flex flex-col items-center">Calendar</div>
+        <div className="lg:col-span-4 md:col-span-2 flex flex-col items-center">
+          <BookingCalendar />
+        </div>
       </section>
     </section>
   );
